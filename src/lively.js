a10470bfea4dc150047c1f8d1dd31d2319a796e9
Brandon G <brandongob@gmail.com>
2018-07-03 23:29:12 -0600

    Changed login on how transform animations work, passing matrices are not recognized for animation anymore.

--- src/lively.js
+++ src/lively.js
@@ -20,55 +20,63 @@
     };
     let customEasings = {};
 
+    function stringContains(string, text) {
+        return string.indexOf(text) > -1;
+    }
     //Helpers
+    const is = {
+        dom: a => a instanceof Element,
+        svg: a => a instanceof SVGElement,
+        obj: a => stringContains(Object.prototype.toString.call(a), 'Object'),
+        array: a => Array.isArray(a),
+        str: a => typeof a === 'string',
+        und: a => a === undefined,
+        fnc: a => typeof a === 'function',
+        rgb: a => /^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i.test(a),
+        hex: a => /(^#[0-9a-f]{3}$)|(^#[0-9a-f]{6}$)/i.test(a),
+        hex3: a => /(^#[0-9a-f]{3}$)/i.test(a),
+        hex6: a => /(^#[0-9a-f]{6}$)/i.test(a),
+        col : a => is.rgb(a) || is.hex(a)
+    };
+
     function getFirstKey(obj) {
-        for (let key in obj) {
-            return key;
-        }
-    }
-    function isElement(object) {
-        return (object instanceof Element);
-    }
-    function isObject(object) {
-        return (object instanceof Object);
+        for (let key in obj) { return key; }
     }
     const livelyProperties = ['update', 'done', 'targets', 'eases', 'preserve', 'keyframes'];
-    // CSS Helpers
     const cssTransformProperties = ['transform', 'translateX', 'translateY', 'rotate', 'scaleX', 'scaleY', 'skewX', 'skewY'];
     const cssColorProperties = ['color', 'background-color', 'border-color', 'fill'];
 
     function parseValue(value, property) {
-        let result = parseColor(value);
-        if (result) return result;
-        result = parseTransform(value, property);
-        if (result !== undefined && !isNaN(result)) return result;
+        if (is.col(value)) return parseColor(value);
+        let result = parseTransform(value, property);
+        if (!is.und(result) && !isNaN(result)) return result;
         result = parseFloat(parseCssValue(value));
-        if (result !== undefined && !isNaN(result)) return result;
+        if (!is.und(result) && !isNaN(result)) return result;
         return value;
     }
     function parseCssValue(value) {
-        if (typeof value !== 'string') return undefined;
+        if (!is.str(value)) return undefined;
         return value.replace(/[^0-9]/g, '');
     }
     function parseTransform(value, property) {
         if (!property || !cssTransformProperties.includes(property)) return undefined;
-        let matrixValues;
-        if (value === 'none') value = 'matrix(1,0,0,1,0,0)';
-        matrixValues = value.match(/\(([^()])+\)/)[0].match(/[^()]+/)[0].split(',');
-        let transform = {
-            scaleX : parseFloat(matrixValues[0]),
-            skewY : parseFloat(matrixValues[1]),
-            skewX : parseFloat(matrixValues[2]),
-            scaleY : parseFloat(matrixValues[3]),
-            translateX : parseFloat(matrixValues[4]),
-            translateY : parseFloat(matrixValues[5]),
-            rotate : Math.round(Math.atan2(parseFloat(matrixValues[1]), parseFloat(matrixValues[0])) * 180/Math.PI)
-        };
-        return transform[property];
+
+
+        let currentTransform = {};
+
+        let match;
+        let regex = /(\w+)\(([^)]*)\)/g;
+        while ((match = regex.exec(value)) !== null) {
+            let number = match[2].replace(/[^0-9]/g, '');
+            if (number) currentTransform[match[1]] = parseFloat(number);
+        }
+
+        if (!currentTransform[property] && stringContains(property, 'scale')) return 1;
+        else if (!currentTransform[property]) return 0;
+        else return currentTransform[property];
     }
     function parseColor(color) {
-        if (typeof color !== 'string') return undefined;
-        if (color.startsWith('rgb(')) {
+        if (is.rgb(color)) {
             let rgb = color.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
             if (rgb) {
                 return {
@@ -78,7 +86,7 @@
                 };
             }
         }
-        else if (color.startsWith('#') && color.length === 4) {
+        else if (is.hex3(color)) {
             let rgb = color.match(/^#([0-9a-f]{3})$/i)[1];
             if (rgb) {
                 return {
@@ -88,7 +96,7 @@
                 };
             }
         }
-        else if (color.startsWith('#') && color.length === 7) {
+        else if (is.hex6(color)) {
             let rgb = color.match(/^#([0-9a-f]{6})$/i)[1];
             if (rgb) {
                 return {
@@ -105,69 +113,64 @@
         }
     }
     function parseUnit(target, property, value) {
-        if (!isElement(target)) return undefined;
-        else return parseCssUnit(target, property, value);
+        if (!is.dom(target)) return undefined;
+        return parseCssUnit(target, property, value);
     }
     function parseCssUnit(element, property, value) {
-        if (isElement(element) && cssColorProperties.includes(property)) return undefined;
-        else if (isElement(element)) {
-            let unit;
-            if (typeof value === 'string') {
-                unit = value.replace(/\d+/, '');
-            }
-            if (!isNaN(value) || unit === '') {
-                let styleValue = getComputedStyle(element).getPropertyValue(property);
-                unit = styleValue.replace(/\d+/, '');
-            }
-            if (unit === '') return undefined;
-            else return unit;
+        if (is.dom(element) && cssColorProperties.includes(property)) return undefined;
+        else if (is.dom(element)) {
+            if (stringContains(property, 'opacity') || stringContains(property, 'scale')) return undefined;
+            let unit = /(%|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|px|pt|pc|deg)$/.exec(value);
+            if (!unit && stringContains(property, 'rotate') || stringContains(property, 'skew')) return 'deg';
+            if (!unit) return 'px';
+            return unit[0];
         }
     }
     function getTargetValue(target, property) {
-        if (isElement(target)) {
+        if (is.dom(target)) {
             return getCssValue(target, property);
         }
         return target[property];
     }
     function getCssValue(element, property) {
-        if (isElement(element) && cssTransformProperties.includes(property)) {
-            return getComputedStyle(element).getPropertyValue('transform');
+        if (is.dom(element) && cssTransformProperties.includes(property)) {
+            return element.style.transform;
         }
-        else if (isElement(element)) {
+        else if (is.dom(element)) {
             return getComputedStyle(element).getPropertyValue(property);
         }
     }
     function setTargetPropertyValue(target, property, value) {
-        if (isElement(target) && cssTransformProperties.includes(property)) {
+        if (is.dom(target) && cssTransformProperties.includes(property)) {
             setTransformValue(target, property, value);
         }
-        else if (isElement(target) && cssColorProperties.includes(property)) {
+        else if (is.dom(target) && cssColorProperties.includes(property)) {
             setRgbValue(target, property, value);
         }
-        else if (isElement(target)) {
+        else if (is.dom(target)) {
             target.style[property] = value;
         }
-        else if (isObject(target)) {
+        else if (is.obj(target)) {
             target[property] = value;
         }
     }
     function setRgbValue(element, property, rgb) {
-        if (isElement(element)) {
+        if (is.dom(element)) {
             element.style[property] = 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
         }
     }
     function setTransformValue(element, property, transform) {
-        if (isElement(element)) {
-            let scaleX = !transform.scaleX ? 1 : transform.scaleX;
-            let skewY = !transform.skewY ? 0 : transform.skewY;
-            let skewX = !transform.skewX ? 0 : transform.skewX;
-            let scaleY = !transform.scaleY ? 1 : transform.scaleY;
-            let translateX = !transform.translateX ? 0 : transform.translateX;
-            let translateY = !transform.translateY ? 0 : transform.translateY;
-            let rotate = !transform.rotate ? 0 : transform.rotate;
-            let matrixString = 'matrix(' + scaleX + ', ' + skewX + ', ' + skewY + ', ' + scaleY + ', ' + translateX + ', ' + translateY + ')';
-            let rotateString = 'rotate(' + rotate + 'deg)';
-            element.style.transform = matrixString + ' ' + rotateString;
+        
+        if (is.dom(element)) {
+
+            let scaleX = !transform.scaleX ? '' : 'scaleX('+ transform.scaleX +') ';
+            let skewY = !transform.skewY ? '' : 'skewY('+ transform.skewY +') ';
+            let skewX = !transform.skewX ? '' : 'skewX('+ transform.skewX +') ';
+            let scaleY = !transform.scaleY ? '' : 'scaleY('+ transform.scaleY +') ';
+            let translateX = !transform.translateX ? '' : 'translateX('+ transform.translateX +') ';
+            let translateY = !transform.translateY ? '' : 'translateY('+ transform.translateY +') ';
+            let rotate = !transform.rotate ? '' : 'rotate('+ transform.rotate +') ';
+            element.style.transform = translateX + translateY + scaleX + scaleY + rotate + skewY + skewX;
         }
     }
     // Animation related constants
@@ -177,19 +180,19 @@
             let animationEases = {
                 'default' : easings.default
             };
-            if (typeof eases === 'function') {
+            if (is.fnc(eases)) {
                 animationEases.default = eases;
             }
-            else if (typeof eases === 'string' && (eases in easings)) {
+            else if (is.str(eases) && (eases in easings)) {
                 animationEases.default = easings[eases];
             }
-            else if (typeof eases === 'string' && (eases in customEasings)) {
+            else if (is.str(eases) && (eases in customEasings)) {
                 animationEases.default = customEasings[eases];
             }
-            else if (eases && eases.length) {
+            else if (is.array(eases)) {
                 for (let i = 0; i < eases.length; i++) {
                     for (let property in eases[i]) {
-                        if ((typeof eases[i][property] === 'string') && (eases[i][property] in easings) || (eases[i][property] in customEasings)) {
+                        if (is.str(eases[i][property]) && (eases[i][property] in easings) || (eases[i][property] in customEasings)) {
                             let easingName = eases[i][property];
                             animationEases[property] = easings[easingName] || customEasings[easingName];
                         }
@@ -208,7 +211,7 @@
                     properties.push(property);
                 }
             }
-            if (typeof targets === 'string') {
+            if (is.str(targets)) {
                 let createdTargets = document.querySelectorAll(targets);
                 for (let i = 0; i < createdTargets.length; i++) {
                     animatedTargets.push({
@@ -217,7 +220,7 @@
                     });
                 }
             }
-            else if (targets.length) {
+            else if (is.array(targets)) {
                 for (let i = 0; i < targets.length; i++) {
                     animatedTargets.push({
                         target : targets[i],
@@ -236,7 +239,7 @@
 
         function parseKeyframes(duration, target, animateObj, property, propertyKeyframes) {
             if (!livelyProperties.includes(property)) {
-                if (typeof animateObj[property] !== 'string' && animateObj[property].length) {
+                if (is.array(animateObj[property])) {
                     propertyKeyframes[property] = [];
                     for (let i = 0; i < animateObj[property].length; i++) {
                         let frame = animateObj[property][i];
@@ -256,7 +259,7 @@
                     // Set the isLast value for the property keyframes
                     propertyKeyframes[property][length - 1].isLast = true;
                 }
-                else if (isObject(animateObj[property])) {
+                else if (is.obj(animateObj[property])) {
                     propertyKeyframes[property] = [];
                     let frame = animateObj[property];
                     let isLast = true;
@@ -321,21 +324,21 @@
             if (keyframe.isLast && currentTime >= keyframe.endTime) {
                 return keyframe.endValue;
             }
-            else if (isElement(target) && cssColorProperties.includes(property)) {
+            else if (is.dom(target) && cssColorProperties.includes(property)) {
                 return {
                     r : tween((currentTime - keyframe.startTime), keyframe.startValue.r, (keyframe.endValue.r - keyframe.startValue.r), (keyframe.endTime - keyframe.startTime)),
                     g : tween((currentTime - keyframe.startTime), keyframe.startValue.g, (keyframe.endValue.g - keyframe.startValue.g), (keyframe.endTime - keyframe.startTime)),
                     b : tween((currentTime - keyframe.startTime), keyframe.startValue.b, (keyframe.endValue.b - keyframe.startValue.b), (keyframe.endTime - keyframe.startTime)),
                 };
             }
-            else if (isElement(target) || isObject(target)) {
+            else if (is.dom(target) || is.obj(target)) {
                 return tween((currentTime - keyframe.startTime), keyframe.startValue, (keyframe.endValue - keyframe.startValue), (keyframe.endTime - keyframe.startTime));
             }
 
         }
         function tick(currentTime, animation) {
-            let duration = animation.duration;
-            let finished = (currentTime >= duration) || (currentTime < 0);
+            const duration = animation.duration;
+            const finished = (currentTime >= duration) || (currentTime < 0);
             if (currentTime < 0) return finished;
 
             let eases = animation.eases;
--- test.js
+++ test.js
@@ -19,15 +19,15 @@ window.onload = function () {
         targets : 'div',
         rotate : [{ '20%' : 180 } , { '85%' : 180 }, { '100%': 360}] ,
         translateX : {'100%' : 200 },
-        scaleX : [{ '20%' : .5 }, { '100%' : 1}],
-        scaleY : [{ '50%' : .5 }, { '100%' : 1}],
+        scaleX : [{ '20%' : .5 }, { '100%' : 2}],
+        scaleY : [{ '50%' : .5 }, { '100%' : 2}],
         //translateY : [{'10%' : 40 }, { '25%' : -40 }, { '100%' : 40 }],
         'opacity' : [{ '25%' : 0.1}, { '50%' : 1}, { '75%' : .5}, {'100%' : 1}],
         'background-color':  [{ '20%' : '#2345ff'}, { '50%' : '#22ff3c'}, { '100%' : '#ffcf21' }],
         'font-size' : [{ '10%' : '50px' }],
         //'background-color' : '#000',
         'color' : '#852234',
-        //'border-radius' :  [{ '0%' : 0 }, {'20%' : '50px'}],
+        //'border-radius' :  [{ '0%' : 0 }, {'20%' : '50%'}],
         eases : [
             { translateX: 'bounce'},
             { rotate: 'bounce' },
@@ -37,6 +37,7 @@ window.onload = function () {
         preserve : true
     }, 5000);
     document.getElementById('mydiv1').onclick = function() {
+
         lively.play();
         setTimeout(function () {
             lively.rewind();
